942a0aa010e8f270eee6712f585ca4c605a22632
ttnppedr <priest0204@gmail.com>
2022-06-30 10:30:28 +0800

    Hello World by oak

---
+++ app.ts
@@ -0,0 +1,16 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const router = new Router();
+router.get("/", (ctx) => {
+  ctx.response.body = "Hello world!";
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener(
+  "listen",
+  (_e) => console.log("Listening on http://localhost:8080"),
+);
+await app.listen({ port: 8080 });
