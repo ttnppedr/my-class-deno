0d06072029537c67f8e8bdfeb8866fcf1b9a67ad
ttnppedr <priest0204@gmail.com>
2022-06-30 18:19:26 +0800

    add mail api

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import "https://deno.land/x/dotenv/load.ts";
+import { Application, Router, Status } from "https://deno.land/x/oak/mod.ts";
 import {
   collection,
   getDocs,
 } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore-lite.js";
 import { db } from "./firebase/firestore.ts";
 import { ClassSchema } from "./firebase/schemas/ClassSchema.ts";
+import ky from "https://cdn.jsdelivr.net/npm/ky@0.31.0/distribution/index.min.js";
 
 const router = new Router();
 router.get("/api/classes", async (ctx) => {
@@ -16,6 +18,34 @@ router.get("/api/classes", async (ctx) => {
   }, this);
 
   ctx.response.body = JSON.stringify(classesArr);
+});
+
+router.post("/api/mail", async (ctx) => {
+  const { email, username } = await ctx.request.body().value;
+
+  await ky.post("https://api.sendinblue.com/v3/smtp/email", {
+    headers: {
+      "api-key": Deno.env.get("SENDINBLUE_KEY"),
+    },
+    json: {
+      sender: {
+        "name": "my-class",
+        "email": "admin@ttnppedr.com",
+      },
+      to: [
+        {
+          email: email,
+          name: username,
+        },
+      ],
+      subject: "hello world",
+      htmlContent:
+        `<html><head></head><body><p>Hello,</p><p>${username}</p></body></html>`,
+    },
+  }).json();
+
+  ctx.response.status = Status.OK;
+  ctx.response.body = "OK";
 });
 
 const app = new Application();
