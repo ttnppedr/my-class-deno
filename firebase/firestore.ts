faad6aef2c5d895728d5286bb7c5f3388619e1c8
ttnppedr <priest0204@gmail.com>
2022-07-01 18:56:41 +0800

    warp environtment variable

--- firebase/firestore.ts
+++ firebase/firestore.ts
@@ -2,6 +2,6 @@ import "https://deno.land/x/dotenv/load.ts";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore-lite.js";
 
-const firebaseConfig = JSON.parse(Deno.env.get("FIREBASE_CONFIG"));
+const firebaseConfig = JSON.parse(`${Deno.env.get("FIREBASE_CONFIG")}`);
 const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
